59b7b5e46a640a1fef5fd3b87600e62b28466fc4
JungKyungSong <lordsong5974@gmail.com>
2023-05-18 18:59:21 +0900

    test

--- LAB4/getProducts.js
+++ LAB4/getProducts.js
@@ -1,15 +1,9 @@
-
-
 function getProducts () {
     fetch("https://2019181052.github.io/HomeworkRepository/LAB4/product.json")
     .then(function(response) {
         return response.json();
     })
 }
-
-/* function displayProducts(products) {
-    
-} */
 
 getProducts().then((products => {
     console.log(products);
